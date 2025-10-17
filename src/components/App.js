b4d8b0a4b5c5a54e6a319c8f233bd983ee03f580
Suleiman <real1sule@gmail.com>
2025-10-17 16:22:52 +0300

    completed hooks state and events

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React,{useState} from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
 
+  //using array destructuring
+    const [darkMode , setDarkMode] = useState(false)
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+
+  //dynamically sets class based on state
+  const appClass = darkMode ? "App dark" : "App light"
+
+  function handleDarkMode () {
+    setDarkMode((prev) => !prev);
+  }
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleDarkMode}>{darkMode ? "Dark Mode":"Light Mode"}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React , {useState} from "react";
+import items from "../data/items";
+
 
 function Item({ name, category }) {
+  const [inInCart, setIsInCart] = useState(false);
+  
+  function handleAddToCart (items) {
+    setIsInCart((prev) => !prev)  
+  }
+   
   return (
-    <li className="">
+    <li className={inInCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleAddToCart}>{inInCart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
+
 }
 
 export default Item;
